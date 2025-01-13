44cf31ba88c44160ba76cf8cce693f53b40a3365
gyash8424 <gyash8424@gmail.com>
2025-01-13 22:09:42 +0530

    data validation pipe created

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from Network_Security.components.data_ingestion import DataIngestion
+from Network_Security.components.data_validation import DataValidation
 from Network_Security.exception.exception import NetworkSecurityException
 from Network_Security.logging.logger import logging
-from Network_Security.entity.corfig_entity import DataIngestionConfig
+from Network_Security.entity.corfig_entity import DataIngestionConfig, DataValidationConfig
 from Network_Security.entity.corfig_entity import TrainingPipelineConfig
 
 
@@ -11,10 +12,20 @@ if __name__ == '__main__':
     try:
         trainingpipelineconfig = TrainingPipelineConfig()
         dataingestionconfig = DataIngestionConfig(trainingpipelineconfig)
+
         data_ingestion = DataIngestion(dataingestionconfig)
 
         logging.info("Initiate the data ingestion")
         dataingestionartifact = data_ingestion.initiate_data_ingestion()
-        print(dataingestionartifact)
+        #print(dataingestionartifact)
+        
+        data_validation_config = DataValidationConfig(trainingpipelineconfig)
+        data_validation = DataValidation(dataingestionartifact, data_validation_config)
+        logging.info("Data Validation initiated")
+
+        data_validation_artifact = data_validation.initiate_data_validation()
+        print(data_validation_artifact)
+        
+        
     except Exception as e:
         raise NetworkSecurityException(e,sys)
